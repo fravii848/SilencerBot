2d20fb882af8b323b9604cf3f83003a114823d0e
fravii848 <51963024+fravii848@users.noreply.github.com>
2019-06-28 18:25:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # coding=utf-8
-admins = [491444575, 106791929]
-token = '566786956:AAEACP90bJLHhl0jrvTcuONCRWsZ_F16G2s'
+admins = [845646704]
+token = '754435984:AAG5px2VywaZHIcY37Nkfse4AEnLud7u_Yg'
 
 menuHelp = 'Инструкция'
 menuChatList = 'Мои чаты'
 
-start_message = 'Добрый день. Для работы со мной:\n1) Добавьте меня в супергруппу\n2) Дайте мне права администратора в этой группе\n'
+start_message = 'ajuda - @Fraviin'
 bot_instruction = 'Как только вы добавите меня в чат, я вас оповещу о том, что чат добавлен в список.\n\nВАЖНО!\n\nУбедитесь, что чат в котором я нахожусь является супергруппой, а так же убедитесь в том, что у меня есть права на удаление сообщений.\nВ ином случае я не смогу вам помочь =/'
 
 all_message_types = ['text', 'sticker', 'document', 'audio', 'animation', 'game', 'video', 'voice', 'video_note', 'contact', 'location', 'venue', 'photo']
